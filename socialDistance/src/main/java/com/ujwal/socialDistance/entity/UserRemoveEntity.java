feec14270c554aef23cf3f86a4412a8d5e6120b4
derEntwickler007 <thedeveloper@0.007@gmail.com>
2020-12-13 16:10:03 +0530

    Summary: Add UserRemoveEntity class

---
+++ socialDistance/src/main/java/com/ujwal/socialDistance/entity/UserRemoveEntity.java
@@ -0,0 +1,23 @@
+package com.ujwal.socialDistance.entity;
+
+public class UserRemoveEntity{
+
+	private Integer userId;
+	private Integer friendId;
+	
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public Integer getFriendId() {
+		return friendId;
+	}
+	public void setFriendId(Integer friendId) {
+		this.friendId = friendId;
+	}
+	
+	
+
+}
